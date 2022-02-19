5fc8570edb0b6871f18bc6b0224959f4994f92c9
jjsa99 <30303154+jjsa99@users.noreply.github.com>
2022-02-19 01:00:54 +0000

    Revert "Update Images"
    This reverts commit a735cce042bcd3ce5c40bb2ff0f4835ea1cf91d3.

---
+++ jpge_png.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+im1 = Image.open("C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/pneumonia/person489_bacteria_2063.jpeg")
+im1.save("C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/pneumonia/person489_bacteria_2063.png")
