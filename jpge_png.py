852849576a9489893ec61d0d9e1bb83560ea2556
jjsa99 <30303154+jjsa99@users.noreply.github.com>
2022-02-19 15:13:24 +0000

    Changed names

--- jpge_png.py
+++ jpge_png.py
@@ -3,14 +3,14 @@ from PIL import Image
 import cv2 as cv
 import numpy as np
 import pandas as pd
-
+import os
 
 
 DATA_PATH = "C:/Users/UX550/Documents/GitHub/FAA_FinalProject/metadata.csv"
-image_path = "C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/pneumonia"
+image_path = "C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/covid"
 
-import os
-rootdir = 'C:/Users/sid/Desktop/test'
+
+
 
 names = []
 for subdir, dirs, files in os.walk(image_path):
@@ -18,8 +18,8 @@ for subdir, dirs, files in os.walk(image_path):
         names.append(os.path.join(subdir, file))
 
 print(names[400][-5:])
- 
+
 for i in range(1526):
-    if(names[i][-5:] == '.jpeg'):
-        im1 = Image.open(str(names[i]))
-        im1.save("C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/pneumonia" +str(i)+ ".png")
+    #if(names[i][-5:] == '.jpeg'):
+    im1 = Image.open(str(names[i]))
+    im1.save("C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/covid/covid_" +str(i)+ ".png")
--- test.py
+++ test.py
@@ -1,42 +1,49 @@
 import cv2 as cv
 import numpy as np
 import pandas as pd
+import os
+from PIL import Image
+# sklearn library 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.metrics import classification_report
 
 
 IMAGE_PATH = "C:/Users/UX550/Documents/GitHub/FAA_FinalProject/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset/"
 DATA_PATH = "C:/Users/UX550/Documents/GitHub/FAA_FinalProject/metadata.csv"
 
-
-# distribution 
-df = pd.read_csv(DATA_PATH)
-
-df["class"].value_counts(normalize = True).plot(kind = 'pie', autopct = "%.1f")
-
-df_covid = df.loc[df["class"] == 1]
-df_normal = df.loc[df["class"] == 0]
-df_pneumonia = df.loc[df["class"] == 2]
-
-# reset the index counter 
-df_covid.reset_index(inplace = True)
-df_covid.drop(["index"], axis = 1, inplace = True)
-df_normal.reset_index(inplace = True)
-df_normal.drop(["index"], axis = 1, inplace = True)
-df_pneumonia.reset_index(inplace = True)
-df_pneumonia.drop(["index"], axis = 1, inplace = True)
-
-df_data = pd.concat([df_normal,df_covid,df_pneumonia],ignore_index = True)
-
 img_size = 150
-data_arr = np.zeros((len(df_data),img_size,img_size))
+data = []
+folders = ["covid","normal","pneumonia"]
+for i in range(len(folders)):
+    image_path = IMAGE_PATH + str(folders[i])
+    print(folders[i])
+    print(image_path)
+    for subdir, dirs, files in os.walk(image_path):
+        for file in files:
+            aux_name = str(subdir) + "/" + str(file)
+            # read image
+            img_arr = cv.imread(aux_name,cv.IMREAD_GRAYSCALE)
+            # resize image
+            resized_arr = cv.resize(img_arr,(img_size,img_size))
+            data.append([resized_arr, i+1])
+            #print(names)
 
-  
-for i in range(len(df_data)):
- image_name = df_data["directory"]
- name = IMAGE_PATH + image_name[i]
- print(name)
- im = cv.imread(name, cv.IMREAD_GRAYSCALE)
- resized_arr = cv.resize(im, (img_size, img_size)) # Reshaping images to preferred size
- data_arr[i] =(resized_arr)
- 
-print(data_arr[2000])
+# convert the list into a dataframe
+data_df = pd.DataFrame(data,columns=['feature','class'])
+
+# randomize the data
+data_rand = data_df.sample(frac = 1)
+
+train_X, test_X, train_Y, test_Y = train_test_split(data_rand['feature'],data_rand['class'],random_state = 50, test_size= 0.4)
+test_X, val_X, test_Y, val_Y = train_test_split(test_X,test_Y,random_state = 50, test_size= 0.5)
+
+# Normalize the data
+x_train = np.array(train_X) / 255
+x_val = np.array(val_X) / 255
+x_test = np.array(test_X) / 255
+
+
+
 
